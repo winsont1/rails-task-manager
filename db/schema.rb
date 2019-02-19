ea0cab84223d460ed629c952ebaf7f2b6545da61
Winson Tan <winsont89@gmail.com>
2019-02-19 11:16:11 +1100

    add tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_000330) do
+ActiveRecord::Schema.define(version: 2019_02_19_001509) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
